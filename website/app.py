b0daed09e9048edeb358929bc650e23b597f71d3
Arul Sinha <85295733+arul-sinha@users.noreply.github.com>
2023-01-12 18:39:59 +0545

    update messages

--- website/app.py
+++ website/app.py
@@ -12,6 +12,10 @@ app = Flask("__name__")
 def index():
     return render_template("index.html")
 
+@app.route('/messages')
+def messages():
+    return render_template("messages.html")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
